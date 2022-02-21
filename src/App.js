031bd99ea356a6a7f9839c6210021c3983e411dc
hanif <h.ehmedov@gmail.com>
2022-02-21 09:32:40 -0500

    git rid of switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //  NPM packages
 import React, { Fragment } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import Main from './components/main/Main'
 import About from './components/main/About'
@@ -9,26 +9,17 @@ import Projects from './components/main/Projects.js'
 const App = () => {
   return (
     <Fragment>
-      <h1>Siktirrrrrrrr</h1>
       <Route exact path='/'>
         <Main />
       </Route>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/about'>
-            <About />
-          </Route>
-          {/* <Route exact path='/experince'>
-      <Experience/>
-    </Route> */}
-          <Route exact path='/projects'>
-            <Projects />
-          </Route>
-          {/* <Route exact path='/resume'>
-      <Resume/>
-    </Route> */}
-        </Switch>
-      </BrowserRouter>
+      
+      <Route exact path='/about'>
+        <About />
+      </Route>
+
+      <Route exact path='/projects'>
+        <Projects />
+      </Route>
     </Fragment>
   )
 }
